fbd8d511061081302f82ab2f96b8d1b1a9eb2565
Coleman Sandy <sandcole@byu.edu>
2025-01-12 15:39:23 -0700

    Initial commit

---
+++ Mission1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var num = int.Parse(args[1]);
+        
+        System.Console.WriteLine("Hello, " + args[0] + "!");
+        System.Console.WriteLine(args[1] + " squared is " + (num * num));
+    }
+}
